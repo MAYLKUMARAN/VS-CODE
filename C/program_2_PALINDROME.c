7abd3dbc59348032ac0490557fbf7abbbf3feeed
MAYLKUMARAN <maylkumaran3@gmail.com>
2024-10-14 11:24:30 +0530

    VERSION 1

--- C/program_2_PALINDROME.c
+++ C/program_2_PALINDROME.c
@@ -4,11 +4,40 @@ int main()
 {
     char string_1[100],string_rev[100];
     fgets(string_1,100,stdin);
-    int rev_count=strlen(string_1);
-    for (int i=0;i<strlen(string_1);i++)
+    int len=strlen(string_1),temp;
+    if(string_1[len-1]=='\n')
     {
-        string_rev[rev_count]=string_rev[i];
-        rev_count--;
+        string_1[len-1]='\0';
+        len--;
     }
-    printf("%s",string)
+    temp=len;
+    for(int i=0;i<len;i++)
+    {
+        if(string_1[i]==' ')
+        {
+            continue;
+        }
+        else
+        {
+        string_rev[temp-1]=string_1[i];
+        temp--;
+        }
+    }
+    printf("%s\n",string_rev);
+    int count=0,len_rev=strlen(string_rev);
+    for (int i=0;i<len;i++)
+    {
+    if(string_1[i]==string_rev[i])
+    {
+        count++;
+    }
+    }
+    if(count==len_rev)
+    {
+        printf("Password is Vulnerable");
+    }
+    else
+    {
+        printf("Password is Non-Vulnerable");
+    }
 }
