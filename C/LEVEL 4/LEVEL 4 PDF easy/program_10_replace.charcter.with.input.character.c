a7f213b84fff4d796cfad3ccc43ba4e802d12386
MAYLKUMARAN <maylkumaran3@gmail.com>
2024-10-18 09:31:03 +0530

    program 10 completed

--- C/LEVEL 4/LEVEL 4 PDF easy/program_10_replace.charcter.with.input.character.c
+++ C/LEVEL 4/LEVEL 4 PDF easy/program_10_replace.charcter.with.input.character.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    char stringa[100];
-    
+    char a[100],b,c='o';
+   // scanf("%[^\n]",a);(doesn't work)
+   fgets(a,100,stdin);  
+    scanf("%c ",&b);
+    scanf("%c",&c);
+
+    for(int i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==b)
+        {
+            a[i]=c;
+        }
+    }
+    printf("%s",a);
 }
