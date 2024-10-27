ac96f7addf7a38f95aad7b33e502cd8ed351a47b
MAYLKUMARAN <maylkumaran3@gmail.com>
2024-10-27 15:29:43 +0530

    PROGRAM COMEPLETED

--- PYTHON/LEVEL 4/trial_prg_classify_the_marks_to_grades.py
+++ PYTHON/LEVEL 4/trial_prg_classify_the_marks_to_grades.py
@@ -1,4 +1,29 @@
-print("Enter the marks of all the subjects")
-for i in range (0,5):
-    a=int(input("Enter the mark of subject {}: ".format(i+1)))
+def average(a):
+    avg=0
+    for i in range(len(a)):
+        avg+=a[i]
+    avg/=len(a)
+    return avg
+def grades(avg):
+    if avg>=80:
+        print("GRADE A")
+    elif avg<80 & avg >=60:
+        print("GRADE B")
+    elif avg<60 & avg >=50:
+        print("GRADE C")
+    elif avg<50:
+        print("GRADE F")
+    print("\n-------------------------------------------------------------------------------")
+    main()
+    
+def main():
+    print("Enter the marks of all the subjects")
+    a=[0]*5
+    for i in range (0,5):
+        a[i]=int(input("Enter the mark of subject {}: ".format(i+1)))
+    avg=average(a)
+    print("Your average is: ",avg)
+    grades(avg)
 
+if __name__=="__main__":
+    main()
