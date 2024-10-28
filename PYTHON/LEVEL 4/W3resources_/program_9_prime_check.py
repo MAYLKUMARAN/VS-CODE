e904d46ce33a147ade0008de69cf554f68eb8360
MAYLKUMARAN <maylkumaran3@gmail.com>
2024-10-28 12:11:21 +0530

    new program added

---
+++ PYTHON/LEVEL 4/W3resources_/program_9_prime_check.py
@@ -0,0 +1,23 @@
+def prime_check(num):
+    for i in range(2,num):
+        if(num%i==0):
+            return False
+        else:
+            continue
+    return True
+
+def main():
+    a=int(input("enter a number for prime check: "))
+    if(prime_check(a)):
+        print("Prime")
+    else:
+        print("Not prime")
+    continue_prompt=input("Do you want to continue again(yes/no): ")
+    if continue_prompt.lower()=="yes":
+        print("=========================================================================================")
+        main()
+    else:
+        print("Program exited")
+        
+if __name__=="__main__":
+    main()
