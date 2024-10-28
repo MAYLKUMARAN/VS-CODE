5a8c6f63e51f61ffceb70b6df5e202bd10e7262a
MAYLKUMARAN <maylkumaran3@gmail.com>
2024-10-28 16:35:22 +0530

    new program added

---
+++ PYTHON/LEVEL 4/W3resources_/program_14_pascals_triangle.py
@@ -0,0 +1,20 @@
+def fact(a):
+    if a==0 or a==1:
+        return 1
+    sum=1
+    for i in range(2,a+1):
+        sum*=i
+        a-=1
+    return sum
+def pascal(i,j):
+    return fact(i)//(fact(i-j)*fact(j))
+def main():
+    a=int(input("Enter the size of the triangle: "))
+    for i in range(a):
+        print()
+        for j in range(a-i+1):
+            print(' ',end='')
+        for j in range(i+1):
+            print(pascal(i,j),' ',end='')
+if __name__=="__main__":
+    main()
