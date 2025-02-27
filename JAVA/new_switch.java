98bedca7e5bf6e07ec774a26771ff7aa82fcaf5b
MAYLKUMARAN <maylkumaran3@gmail.com>
2025-02-27 20:54:14 +0530

    deleted test and includede new switch

---
+++ JAVA/new_switch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class new_switch
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        switch(a)
+        {
+            case 1 ->System.out.print("Monday");
+            default ->System.out.print("mention 1");
+        }
+    }
+}
