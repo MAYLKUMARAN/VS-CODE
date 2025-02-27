fd3d1a1a3121055bb643e10bf9548e8abc794457
MAYLKUMARAN <maylkumaran3@gmail.com>
2025-02-27 18:09:31 +0530

    nothing serious

---
+++ JAVA/test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class test
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        
+        System.out.print(a);
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int n= input.next().charAt(0);
+        System.out.print(n);
+    }
+}
