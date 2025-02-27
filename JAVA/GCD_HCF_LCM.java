81b5cd85af425bf2c0650e8065c8f051259d07d7
MAYLKUMARAN <maylkumaran3@gmail.com>
2025-02-27 21:49:43 +0530

    NIL

--- JAVA/GCD_HCF_LCM.java
+++ JAVA/GCD_HCF_LCM.java
@@ -3,7 +3,10 @@ public class GCD_HCF_LCM {
     public static void main(String[] args)
     {
         Scanner input= new Scanner(System.in);
+        System.out.println("PROGRAM TO FIND GCD AND LCM");
+        System.out.print("\t Enter a value 1: ");
         int a=input.nextInt();
+        System.out.print("\t Enter a value 2: ");
         int b=input.nextInt();
         int val=0;
         if(a<b)
@@ -23,6 +26,10 @@ public class GCD_HCF_LCM {
                 GCD_val=i;
                 break;
             }
+            i--;
         }
+        System.out.println("The GCD is: "+GCD_val);
+        System.out.println("The LCM is: "+a*b/GCD_val);
+        input.close();
     }
 }
